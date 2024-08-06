39ea62cc005f5800096dc8586d79bb20137f26f4
abdullahshinwari <abdullah@thespotters.net>
2024-08-06 10:46:56 -0400

    initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Chai from "./chai"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Chai</h1>
+      <Chai />
+      <p>Biscuit is coming...</p>
+    </>
+  )
+}
+
+export default App
