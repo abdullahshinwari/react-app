1b158a49654783a2568693d565bf3a7e4acba6ee
abdullahshinwari <abdullah@thespotters.net>
2024-10-18 12:50:59 -0400

    add react crud

---
+++ reactCrud/src/EmployeeData.js
@@ -0,0 +1,20 @@
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName: 'Leonal',
+        lastName: 'Messi',
+        age: 33
+    },
+    {
+        id: 2,
+        firstName: 'shoaib',
+        lastName: 'Akhtar',
+        age: 50
+    },
+    {
+        id: 3,
+        firstName: 'Babar',
+        lastName: 'Azam',
+        age: 28
+    }
+]
